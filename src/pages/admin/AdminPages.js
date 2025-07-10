574b2fef2f2e9537ab3bb04beafec53c102b1769
Drakuu <fahadrah819@gmail.com>
2025-07-10 10:28:25 +0500

    some layout

--- src/pages/admin/AdminPages.js
+++ src/pages/admin/AdminPages.js
@@ -1,6 +1,5 @@
 import AdminDashboard from "./AdminDashboard";
-import AdminPaitient from "./adminPaitient/AdminPaitientOverview"
-import AddNew from "./adminPaitient/AddNew"
 
-
-export{AdminDashboard,AdminPaitient,AddNew};
+export { 
+    AdminDashboard, 
+    };
